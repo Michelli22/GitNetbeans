e3ec52bc7a5a9f97c9485e0dcb17b8a3f729260a
Michelli <a18102313@idat.edu.pe>
2019-10-30 11:38:30 -0500

    + Mensaje hhhhh

--- src/main/java/pe/edu/idat/daa2/app/App.java
+++ src/main/java/pe/edu/idat/daa2/app/App.java
@@ -5,7 +5,7 @@ public class App {
     
     public static void main(String[] args) {
         
-        System.out.println("Hola gente mmm");
+        System.out.println("Hola gente mmmm");
     }
     
 }
